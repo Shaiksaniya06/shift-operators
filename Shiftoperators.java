47c516be972af881c03b4a5dfd2f5a536cc1b7f7
Shaiksaniya06 <sksaniya2006@gmail.com>
2025-01-26 18:39:33 +0530

    Add files via upload

---
+++ Shiftoperators.java
@@ -0,0 +1,16 @@
+public class Shiftoperators {
+    public static void main(String[] args) {
+    //     int num =12;
+    //     int rightshift=num >>1;
+    //     System.out.println("num:" + rightshift);
+    
+
+  int num=7;
+  int leftshift=num<<1;
+      System.out.println("num:" + leftshift);
+    }
+}
+
+
+    
+
